533f10aff225fec2c9f56ec8f7cfc831f3e96b3f
mansan2000 <emanuelsanderscode@gmail.com>
2023-01-13 08:25:27 -0600

    adding lab 1 code

---
+++ lab01/lab01.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+
+int isPrime(int givenNumber){
+	int total = 0;
+    for (int i = 1; i < givenNumber; i++){
+        if (givenNumber%i==0){
+			total += i;
+        }
+    }
+	printf("Total: %d\n",total);
+	if(total == 1){
+		printf("The given number is a prime\n");
+	} else{
+		printf("The given number is not a prime\n");
+	}
+	return 1;
+
+}
+
+
+int main(){
+	
+    // Take user input and then call funtion using the input
+    printf("========= UABNumber =========\n");
+    int n2;
+    printf("Enter an integer for givenNumber: ");
+    scanf("%d", &n2);  
+	isPrime(n2);
+
+
+}
