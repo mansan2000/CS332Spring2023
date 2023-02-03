108dc732a1f058ecd068e0739db97fd330bfc636
mansan2000 <emanuelsanderscode@gmail.com>
2023-02-03 10:19:53 -0600

    adding lab04

---
+++ lab04/lab04.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define	BUFFSIZE 4096
+
+int main(int argc, char *argv[]) {
+    int  readFileDescriptor, writeFileDescriptor;
+    long int n;
+    char buf[BUFFSIZE];
+
+    if (argc != 3){
+        printf("Usage: %s <source_filename> <destination_filename>\n", argv[0]);
+        exit (-1);
+    }
+
+
+    if(strcmp(argv[1], argv[2]) == 0){
+        printf("Error: Both files are the same\n");
+        exit(-1);
+    }
+
+    readFileDescriptor = open(argv[2], O_RDONLY);
+    writeFileDescriptor = open(argv[1], O_CREAT|O_WRONLY|O_APPEND, 0700);
+
+    if (readFileDescriptor == -1 || writeFileDescriptor == -1){
+        printf("Error with file open\n");
+        exit (-1);
+    }
+
+    char c[] = {"\n"};
+    write(writeFileDescriptor, &c, 1 );
+    write(writeFileDescriptor, &c, 1 );
+    while ((n = read(readFileDescriptor, buf, BUFFSIZE)) > 0){
+        if (write(writeFileDescriptor, buf, n) != n){
+            printf("Error writing to output file\n");
+            exit (-1);
+        }
+    }
+    if (n < 0){
+        printf("Error reading input file\n");
+        exit (-1);
+    }
+
+    close(readFileDescriptor);
+    close(writeFileDescriptor);
+
+    return 0;
+
+}
